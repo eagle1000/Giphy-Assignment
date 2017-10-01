da726ea032a5ffc08cd4fd2284b0d48d86dbea20
Brett <brettartmann@gmail.com>
2017-10-01 13:07:35 -0500

    second version

--- javascript/script.js
+++ javascript/script.js
@@ -9,7 +9,7 @@
 var topics = ["shark", "tiger", "lion", "monkey", "parrot", "seal", "bear", "frog", "dog", "cat"]
 
 var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        "tiger" + "&api_key=dc6zaTOxFJmzC&limit=10";
+        "tiger" + "&api_key=dc6zaTOxFJmzC&limit=10";  //hardcoded to tiger
 
 //Functions
 
@@ -21,10 +21,47 @@ var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         // After the data comes back from the API
         .done(function(response) {
           // Storing an array of results in the results variable
-          var results = response.data;
-          console.log(results);
+          var results = response.data[0].url;
+          console.log(results);//hardcoded to element [0]
+          renderButtons();
+
+ // This function handles events where one button is clicked
+      $("#addAnimal").on("click", function(event) {
+        // event.preventDefault() prevents the form from trying to submit itself.
+        // We're using a form so that the user can hit enter instead of clicking the button if they want
+        event.preventDefault();
+   // This line will grab the text from the input box
+        var newAnimal = $("#animal-input").val().trim();
+        // The animal from the textbox is then added to our array
+        topics.push(newAnimal);
+        renderButtons();
       });
+ });
+  // Function for displaying animal buttons
+      function renderButtons() {
 
+        // Deleting the movie buttons prior to adding new movie buttons
+        // (this is necessary otherwise we will have repeat buttons)
+        $("#animalButtons").empty();
 
+        // Looping through the array of movies
+        for (var i = 0; i < topics.length; i++) {
 
-//Startup/Main Logic
+          // Then dynamicaly generating buttons for each movie in the array.
+          // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
+          var newButton = $("<button>");
+          // Adding a class
+          newButton.addClass("animalBtn");
+          // Adding a data-attribute with a value of the movie at index i
+          newButton.attr("data-name", topics[i]);
+          // Providing the button's text with a value of the movie at index i
+          newButton.text(topics[i]);
+          // Adding the button to the HTML
+          $("#animalButtons").append(newButton);
+        }
+      }
+
+     
+
+    
+     
